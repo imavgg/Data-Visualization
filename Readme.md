0f0bce900fd0953e0a7e4c6f21c5f0c6c7486983
HXWXH <103260434+HXWXH@users.noreply.github.com>
2022-04-18 23:32:42 +0800

    Add files via upload

--- Readme.md
+++ Readme.md
@@ -8,6 +8,51 @@
 3. Ngrok轉外部HTTP可取
     * Download [ngrok.exe](https://dashboard.ngrok.com/get-started/setup), and login to make authtoken first(must!!!)
 
+<<<<<<< HEAD
     * Run command in ngrok command line:
     `ngrok authtoken {$your_token_from_pc}`
     `ngrok.exe http 5000` 
+=======
+## Git 首次下載:
+* git clone https://github.com/imavgg/Data-Visualization.git 需先下載本端。
+* 複製（Fork）一份原作的專案到你自己的 GitHub 帳號底下。
+* cd Data-Visualization
+* git checkout -b "your branch name" :設一個新的branch 名字。
+
+## 開發本地專案
+
+* 請在自己forked的專案下獨立開發，確認無誤再往下走以Git推送至此。
+
+## Git 推送
+<<<<<<< HEAD
+* git checkout "your branch" : 切換到自己設定的分支下。
+* git remote -v : 查看自己推送的遠端位子名稱 ---> 會出現類似以下(遠端名可能是Data-Visualization 但也可能出現別的ex:origin)
+
+```
+Data-Visualization     https://github.com/imavgg/Data-Visualization.git (fetch)
+Data-Visualization      https://github.com/imavgg/Data-Visualization.git (push)
+```
+* git pull "遠端名"  "your branch name" : 需要先讓本地端版本與git上一致
+* git add . :增加內容
+* git status :查看改變內容
+* git commit -m  "your words" :說明推送文字
+
+* git push "遠端名" "your branch name" :推上你的BRANCH，可於此github上確認以及檢察。
+
+
+* 接下來，我會收到你每次的推送紀錄，會以Pull request方式合併專案。
+
+## 其他可能用到的指令:
+* git branch -a : 查看全部遠端branch (如果忘記自己的branch)
+=======
+* git checkout "your branch" : 切換到自己設定的分支下
+* git add . 增加內容
+* git status 查看改變內容
+* git commit -m  "your words" 說明推送文字
+* git push origin "your branch name" 推上你的BRANCH
+
+## 其他可能用到的指令:
+* git remote -v : 查看自己推送的遠端
+* git branch -a : 查看全部遠端branch
+>>>>>>> 5c1f8fe9ee31513cd2becc7df9de3bb843854b03
+>>>>>>> 3676c1010759da7138f3bd475257036e214d08e0
--- app.py
+++ app.py
@@ -8,26 +8,61 @@ from sqlite3 import connect
 import json,pickle
 import pandas as pd
 from pyparsing import col
-import pickle_model0  as xdart # x2articles
-CLOTHS_FOLDER = os.path.join('static','cloths')
-app = Flask(__name__)
+from Model_1 import pickle_model  as x2art # x2articles
+from Model_1 import pickle_model0  as xdart # x2articles
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = [DB_TYPE]+[DB_CONNECTOR]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
+
+CLOTHS_FOLDER = os.path.join('static', 'cloths')
+JSON_FOLDER = os.path.join('static', 'data')
+
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DevDb.db'
 app.config['CLOTHS_FOLDER'] = CLOTHS_FOLDER
 app.config['SECRET_KEY'] = "random string"
-
+app.config['JSON_FOLDER'] = JSON_FOLDER
 db = SQLAlchemy(app)
 mail = Mail(app)
+
+
+# this is a test for printing images on recommend
+prediction =[]
+def test():
+    from random import sample 
+    # The default random articles ID for ML
+    IDs = ['0108775015','0108775044','0108775051','0110065001','0110065002','0110065011','0111565001','0111565003','0111586001','0111593001','0111609001','0112679048','0112679052','0114428026','0114428030','0116379047','0118458003','0118458004','0118458028','0118458029','0118458034','0118458038','0118458039']
+    cloth_files=[]
+    picked = sample(IDs,12)
+    for DXX in picked:
+        Name = str(DXX) + '.jpg'
+        Folder = str(DXX)[0:3]
+        FN = Folder +'\\'+ Name
+        cloth_files.append(FN)
+
+    # return 給HTML的參數
+    files=[]
+    for i in cloth_files:
+        file = app.config['CLOTHS_FOLDER']+ '\\'+ i
+        # print(file)
+        files.append(file)
+    return(files)
+files = test()
+
 class Appmail():
-    def __init__(self,title,sender,body):
-        self.title= title
+    def __init__(self, title, sender, body):
+        self.title = title
         self.sender = sender
         self.body = body
 
-# add item into database
+# add user input item into database
 class AppInfo(db.Model):
-    id = db.Column('id',db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    gender = db.Column(db.String)
+    age = db.Column(db.String)
+    club = db.Column(db.String)
+    fnews = db.Column(db.String)
+
+    # FOR MODEL1
     sales_channel_id = db.Column(db.String)
     product_group_name= db.Column(db.String)
     graphical_appearance_name= db.Column(db.String)
@@ -40,9 +75,16 @@ class AppInfo(db.Model):
     salelabel= db.Column(db.String)
     PREDICT = db.Column(db.String) #save predict result
 
-    def __init__(self,sales_channel_id,product_group_name,
-    graphical_appearance_name,colour_group_name,section_name,garment_group_name,
-    Year,season,pricelabel,salelabel,PREDICT):
+    # FOR MODEL2
+
+    def __init__(self, name, gender, age, club, fnews,sales_channel_id,
+    product_group_name,graphical_appearance_name,colour_group_name,
+    section_name,garment_group_name,Year,season,pricelabel,salelabel,PREDICT):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.club = club
+        self.fnews = fnews
         self.sales_channel_id=sales_channel_id
         self.product_group_name=product_group_name
         self.graphical_appearance_name=graphical_appearance_name
@@ -57,15 +99,20 @@ class AppInfo(db.Model):
 
 # main link
 @app.route('/', methods=['GET', 'POST'])
-
 def main_page():
     '''
-    if request.method == 'POST':
-        if not request.form['sales_channel_id'] or not request.form['product_group_name'] or not request.form['graphical_appearance_name']:
+    # 接收UI輸入
+    if request.method == 'POST':  # pass with http
+        if not request.form['name'] or not request.form['gender'] or not request.form['age']:
             flash('Please enter all the fields', 'error')
         else:
             # 從 HTML 得到參數 並 新增info欄位
-            info = AppInfo(request.form['sales_channel_id'],
+            info = AppInfo(request.form['name'],
+                           request.form['gender'],
+                           request.form['age'],
+                           request.form['club'],
+                           request.form['fnews'],
+                           request.form['sales_channel_id'],
                            request.form['product_group_name'],
                            request.form['graphical_appearance_name'],
                            request.form['colour_group_name'],
@@ -77,16 +124,90 @@ def main_page():
                            request.form['salelabel'],
                            'NONE'
                            )
+            # 加進DB
             db.session.add(info)
             db.session.commit()
             flash('Record was successfully added')'''
+    #return render_template('data/show_all.html', appInfo=AppInfo.query.all())
     return render_template('data/show_all.html')
 
-#視覺化
-@app.route('/vis',methods=['GET', 'POST']) 
-def visualization():
-    return render_template('data/vis.html', appInfo=AppInfo.query.all())
 
+# Restful接收data
+@app.route('/data_pie')
+def data_pie():
+        
+    # 輸入json
+    file1 = open(JSON_FOLDER+'\\garment_group_name.json')
+    file2 = open(JSON_FOLDER+'\\perceived_colour_value_name'+'.json')
+    
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+    #  判別 html 傳回的plot columns項目
+    pie = request.args.get('pie')
+    data_js=data1
+    # import file
+    if pie == 'garment_group_name':
+        data_js = data1
+
+    elif pie == 'perceived_colour_value_name':
+        data_js = data2
+
+    else:
+        print("nothing")
+    # ?/data_pie?pie=garment_group_name  ---> return data.js
+    # return a string. 
+    return( json.dumps(data_js))
+
+@app.route('/data_bar')
+def data_bar():
+        
+    file1 = open(JSON_FOLDER+'\\bar\\index_group_perceived_colour_value_name'+'.json')
+    file2 = open(JSON_FOLDER+'\\bar\\index_group_product_group_name'+'.json')
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+    #  判別 html 傳回的plot columns項目    
+    bar = request.args.get('bar')
+    print('bar',bar)
+    data_js=data1
+    # import file
+    if bar == 'index_group_product_group_name':
+        data_js = data2
+    elif bar == 'index_group_perceived_colour_value_name':
+        data_js = data1
+    else:
+        print("nothing")
+    return( json.dumps(data_js))
+
+@app.route('/data_timeline')
+def data_timeline():
+        
+    file1 = open(JSON_FOLDER+'\\time\\dateprice.json')
+    file2 = open(JSON_FOLDER+'\\time\\dattoaccu.json')
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+    #  判別 html 傳回的plot columns項目    
+    timeline = request.args.get('timeline')
+    print('timeline',timeline)
+    data_js=data1
+    # import file
+    if timeline == 'dateprice':
+        data_js = data1
+    elif timeline == 'dattoaccu':
+        data_js = data2
+    else:
+        print("nothing")
+    return( json.dumps(data_js))
+
+# 視覺化接收選單name
+@ app.route('/vis', methods=['GET', 'POST'])
+def visualization():
+    
+    pie = request.args.get('pie')
+    bar = request.args.get('bar')  
+    timeline = request.args.get('timeline')    
+    globalc = request.args.get('globalc')
+    
+    return render_template('data/vis.html', args=[pie,bar,timeline,globalc] )
 # 關於我們
 @app.route('/us',methods=['GET', 'POST'])
 def us():
@@ -111,34 +232,7 @@ def us():
 # 推薦產品頁面
 @app.route('/recommend', methods=['GET', 'POST'])
 def recommend():
-    '''
-    # 這裡引入folder 內圖檔
-    from random import sample #假裝是機器學習 XDD
-    ls = ['0108775015','0108775044','0108775051','0110065001','0110065002','0110065011','0111565001','0111565003','0111586001','0111593001','0111609001','0112679048','0112679052','0114428026','0114428030','0116379047','0118458003','0118458004','0118458028','0118458029','0118458034','0118458038','0118458039']
-    lls = []
-    R = sample(ls,12)
-    for DXX in R:
-        N = str(DXX) + '.jpg'
-        F = str(DXX)[0:3]
-        FN = F +'/'+ N
-        lls.append(FN)
-    B = lls
-    #B = 'an'+'/'+'test'+'.jpg' #無須另加''  (= = 搞到我呀~小哥哥)
-    print("B =",B)
-    #files = os.path.join(app.config['CLOTHS_FOLDER'],'an/test.jpg')
-    files1 = os.path.join(app.config['CLOTHS_FOLDER'],B[0])
-    files2 = os.path.join(app.config['CLOTHS_FOLDER'],B[1])
-    files3 = os.path.join(app.config['CLOTHS_FOLDER'],B[2])
-    files4 = os.path.join(app.config['CLOTHS_FOLDER'],B[3])
-    files5 = os.path.join(app.config['CLOTHS_FOLDER'],B[4])
-    files6 = os.path.join(app.config['CLOTHS_FOLDER'],B[5])
-    files7 = os.path.join(app.config['CLOTHS_FOLDER'],B[6])
-    files8 = os.path.join(app.config['CLOTHS_FOLDER'],B[7])
-    files9 = os.path.join(app.config['CLOTHS_FOLDER'],B[8])
-    files10 = os.path.join(app.config['CLOTHS_FOLDER'],B[9])
-    files11 = os.path.join(app.config['CLOTHS_FOLDER'],B[10])
-    files12 = os.path.join(app.config['CLOTHS_FOLDER'],B[11])
-    '''
+    # rec html 讀取files
     select1 = request.form.get('sales_channel_id')
     select2 = request.form.get('product_group_name')
     select3 = request.form.get('graphical_appearance_name')
@@ -161,31 +255,30 @@ def recommend():
     B = lls
     #y1 = y1[0]
     y1 = os.path.join(app.config['CLOTHS_FOLDER'],B[0])
-   
     return render_template('data/rec.html', prediction_text='{}.jpg'.format(y1))
 
 
-
-# 依照各ID  連結去推薦產品頁面
-@app.route('/predict/<app_id>', methods=['GET', 'POST'])
+# 依照各FORM ID  連結去推薦產品頁面
+@app.route('/recommend/<app_id>', methods=['GET', 'POST'])
 def modify(app_id):
     if request.method == 'GET':  
         if not app_id:
             flash('Please enter the fields', 'error')
         else:
-            # 從資料庫撈出產品
+            # 從資料庫撈出某一欄位顧客資訊
             appInfo = db.session.query(AppInfo).filter_by(id=app_id).first()
-            print('appInfo.name = ',appInfo.name)
-            print('appInfo.Gender = ',appInfo.gender)
-            print('appInfo.Age = ',appInfo.age)
-            print('appInfo.Club = ',appInfo.club)
-            print('appInfo.Fashion News = ',appInfo.fnews)
-            print(type(appInfo.age))
 
-            #print('appInfo =',appInfo)
-            flash('Record was successfully delete')
-    return redirect(url_for('recommend'))
+            # AI Model 1 :產生一個article id
+            ex1 =x2art.encode(appInfo)
+            y1 = x2art.predict(ex1)         
 
+            # save to model
+            appInfo.PREDICT =str(y1)
+            db.session.add(appInfo)    
+            db.session.commit()
+   
+
+    return render_template('data/rec.html', prediction_text='{}.jpg'.format(y1))
 
 
 
